dfc3ef7d3588555b931b5adc7efc60ced5a7df9e
Mashopy <eliasgheeraert@gmail.com>
2026-08-11 20:10:27 -0500

    mt6879-common: Build common chipinfo
    Change-Id: I69e62472d197bac5b24813065c823bd919826f6c

--- mt6879.mk
+++ mt6879.mk
@@ -116,6 +116,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Chipinfo
+PRODUCT_PACKAGES += \
+    chipinfo
+
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
