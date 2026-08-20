3282e10b2fbf8dcd5e5a66dec2b558851370ba84
Rexix01 <rexix01@proton.me>
2026-08-20 22:19:52 +0200

    Revert "manaus: Switch to source-built C2 service"
    This reverts commit 27f4a13167ced023f9c870cc076b75af92e35b81.
    
    Change-Id: I343e706bd2c1eae9a239618272ae6491fac38abd

--- mt6879.mk
+++ mt6879.mk
@@ -212,9 +212,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Media
-PRODUCT_PACKAGES += \
-    android.hardware.media.c2-mtk-service
-
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(COMMON_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
 
